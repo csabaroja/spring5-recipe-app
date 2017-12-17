b1642a6c576ce75d046f2493abc55fd138959c6d
Csaba Roja <csabaroja@gmail.com>
2017-12-17 15:56:50 +0100

    added difficulty

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -17,7 +17,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -27,6 +26,9 @@ public class Recipe {
 
     @OneToOne( cascade = CascadeType.ALL )
     private Notes notes;
+
+    @Enumerated( value = EnumType.STRING )
+    private Difficulty difficulty;
 
     public Long getId() {
         return id;
@@ -114,5 +116,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
